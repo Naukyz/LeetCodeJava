055f924654788f6340a1d4d970e03a8ee3ec4bd9
Naukyz <zhao_yukuan@163.com>
2018-01-08 21:42:15 +0800

    180108

--- src/LeetCode/L28/L28.java
+++ src/LeetCode/L28/L28.java
@@ -1,5 +1,16 @@
 package LeetCode.L28;
 
+/*
+28. Implement strStr()
+Implement strStr().
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+Example 1:
+Input: haystack = "hello", needle = "ll"
+Output: 2
+Example 2:
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+ */
 class Solution {
     public int strStr(String haystack, String needle) {
         if (haystack.equals(needle) || needle.length() == 0) {
