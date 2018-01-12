714c6769c5bd8bf63079bea5cfda325392aded1f
Naukyz <zhao_yukuan@163.com>
2018-01-12 10:58:59 +0800

    180109

--- src/LeetCode/test/Test.java
+++ src/LeetCode/test/Test.java
@@ -1,11 +1,14 @@
 package LeetCode.test;
 
+import java.util.Arrays;
+
 /**
  * Created by zhao_yukuan@163.com
  * on 2018/1/8
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello ");
+        String[] a = {"a", "aa"};
+        System.out.println(Arrays.toString(Arrays.copyOfRange(a, 0, 2)));
     }
 }
